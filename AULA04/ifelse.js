a81522cc885998ac9b89a9376dd59579ae1d72b5
Gracielle Fabri <fabrigracielle7@gmail.com>
2024-10-14 16:57:02 -0300

    ifelse

---
+++ AULA04/ifelse.js
@@ -0,0 +1,16 @@
+let notaAluno = 8.5
+
+//se(condição){bloco de código}
+if (notaAluno >= 7) {
+    console.log("Aluno Aprovado!");
+}
+
+//senão a condição inicial, mas se (condições){bloco de código}
+else if (notaAluno > 6) {
+    console.log("Aluno Aprovado pelo conselho.");
+}
+
+//senão for nenhuma das condições{bloco de código}
+else {
+    console.log("Aluno Reprovado!");
+}
